2de9a5e68ba1827dabc8a19652efac181cc35f6d
Arunabho Bhattacharya <barunabho@gmail.com>
2024-05-19 21:29:00 +0530

    added dark/light mode

---
+++ script.js
@@ -0,0 +1,6 @@
+let daynight = document.querySelector(".daynight");
+let banner = document.querySelector(".banner");
+
+daynight.addEventListener("click", ()=>{
+    banner.classList.toggle("night")
+})
