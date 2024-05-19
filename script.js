365b8203202fdd28f61ad415bad187ed3eadbea0
Arunabho Bhattacharya <barunabho@gmail.com>
2024-05-19 21:53:30 +0530

    added auto typing effect

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let banner = document.querySelector(".banner");
 daynight.addEventListener("click", ()=>{
     banner.classList.toggle("night")
 })
+
+let typingEffect = new Typed("#text", {
+    strings:["DropXOut","Coder","Developer"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000
+})
